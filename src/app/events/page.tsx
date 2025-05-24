bb37a9911e8f3350524c66d9efd59cd9afa4d6c2
Arthur Pestana <92184381+arthurpestana@users.noreply.github.com>
2025-05-24 11:47:52 -0300

    Wrap EventContent in Suspense for improved loading state handling

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -4,14 +4,17 @@ import { HeaderContent } from '@/components/structure/HeaderContent';
 import styles from './page.module.scss';
 import { NavBar } from '@/components/structure/NavBar';
 import { EventContent } from './_ui/EventContent';
+import { Suspense } from 'react';
 
-export default function EventsPage(){
+export default function EventsPage() {
     return (
         <div className={styles.events__page}>
-            <HeaderContent height='100vh' style={{ backgroundImage: 'url(/images/banner.png)'}}>
-                <NavBar/>
+            <HeaderContent height='100vh' style={{ backgroundImage: 'url(/images/banner.png)' }}>
+                <NavBar />
             </HeaderContent>
-            <EventContent/>
+            <Suspense fallback={<div>Carregando eventos...</div>}>
+                <EventContent />
+            </Suspense>
         </div>
     );
 }
